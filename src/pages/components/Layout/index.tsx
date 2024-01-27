650bfeaaa7ab282b1437dd2de5fb3e61908fa97f
Ingra Dantona <ingradantona.arq@gmail.com>
2024-01-27 18:42:08 -0400

    feat: create menu animation

--- src/pages/components/Layout/index.tsx
+++ src/pages/components/Layout/index.tsx
@@ -1,9 +1,22 @@
 import { Outlet } from "react-router-dom";
+import { Content, Menu, Page } from "./styles";
+import { useState } from "react";
 
 export function Layout() {
+  const [isOpen, setIsOpen] = useState(true);
+
   return (
     <>
-      <Outlet />
+      <Page>
+        <Menu isOpen={isOpen}>
+          <button onClick={() => setIsOpen(!isOpen)}>
+            {isOpen ? "fechar" : "abrir"}
+          </button>
+        </Menu>
+        <Content>
+          <Outlet />
+        </Content>
+      </Page>
     </>
   );
 }
--- src/pages/components/Layout/styles.ts
+++ src/pages/components/Layout/styles.ts
@@ -1,69 +1,60 @@
+import { motion } from "framer-motion";
 import styled from "styled-components";
 
-export const Menu = styled.section`
-  overflow-y: auto;
-  width: 100%;
-  height: 100%;
+export const Page = styled.div`
+  width: 100vw;
+  display: flex;
+`;
+
+interface IPageProps {
+  isOpen?: boolean;
+}
+
+export const Menu = styled(motion.div).attrs<IPageProps>(({ isOpen }) => ({
+  variants: {
+    open: {
+      width: "22vw",
+      transition: {
+        duration: 0.5,
+        type: "spring",
+        damping: 13,
+      },
+    },
+    close: {
+      width: "10vw",
+      transition: {
+        duration: 0.5,
+        type: "spring",
+        damping: 13,
+      },
+    },
+  },
+  initial: "close",
+  animate: isOpen ? "open" : "close",
+}))`
+  height: 100vh;
+  background-color: ${({ theme }) => theme.colors.background.darkGray};
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
-  gap: 1.5vh;
-  overflow: hidden;
-
-  button {
-    border: none;
-    background-color: transparent;
-    width: auto;
-  }
-
-  .link {
-    color: ${({ theme }) => theme.colors.typography.white};
-    font-family: "Verdana", sans-serif;
-    font-weight: 200;
-    height: 100%;
-    text-decoration: none;
-    transition: all 0.2s ease;
-
-    /* ::before {
-      content: "";
-      height: 10vh;
-      width: 5px;
-      border-radius: 5px;
-      transition: all 0.2s ease;
-      position: absolute;
-      left: 0;
-    } */
-
-    &:hover {
-      background-color: ${({ theme }) => theme.colors.background.grayHover};
-    }
-
-    .link-text {
-      margin-left: 1.5vw;
-      white-space: nowrap;
-      font-weight: 200;
-      font-size: clamp(0.15rem, 0.23rem + 1.5vh, 5rem);
-    }
-    .active {
-      font-weight: 600;
-    }
-  }
-
-  .active {
-    background-color: ${({ theme }) => theme.colors.background.graySelected};
-    color: ${({ theme }) => theme.colors.secondary.main};
-    border-left: 5px ${({ theme }) => theme.colors.secondary.main} solid;
-
-    .link-text {
-      font-weight: 600;
-    }
-
-    /* ::before {
-      background-color: ${({ theme }) => theme.colors.secondary.main};
-    } */
-
-    svg {
-      stroke: ${({ theme }) => theme.colors.secondary.main};
-    }
-  }
+  align-items: center;
+  position: relative;
+  overflow-y: auto;
 `;
+
+export const Content = styled(motion.main).attrs(() => ({
+  variants: {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 1,
+        type: "ease",
+      },
+    },
+  },
+  initial: "hidden",
+  animate: "visible",
+}))``;
